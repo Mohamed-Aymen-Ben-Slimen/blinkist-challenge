8a8b774dab9dc31f758b25413a8876b38058f2ee
Mohamed Aymen Ben Slimen <mohamed.aymen.ben.slimen@gmail.com>
2024-03-07 21:05:51 +0100

    update data.ts

--- src/app/api/data.ts
+++ src/app/api/data.ts
@@ -21,20 +21,20 @@ interface EventRecord {
 }
 
 export const getPageViewData = async (): Promise<PageViewRecord[]> => {
-  return (await kv.get("pageViewData")) ?? [];
+  const res = await kv.get<PageViewRecord[]>("pageViewData");
+  return res ?? [];
 };
 
 export const pushPageViewData = async (record: PageViewRecord) => {
-  console.log(kv);
   let pageViewData = await getPageViewData();
   pageViewData?.push(record);
-  const res = await kv.set("pageViewData", pageViewData);
-  console.log(res);
+  console.log("pageViewData", record, pageViewData);
+  await kv.set("pageViewData", pageViewData);
 };
 
-// Define functions to interact with eventData
 export const getEventData = async (): Promise<EventRecord[]> => {
-  return (await kv.get("eventData")) ?? [];
+  const res = await kv.get<EventRecord[]>("eventData");
+  return res ?? [];
 };
 
 export const pushEventData = async (record: EventRecord) => {
